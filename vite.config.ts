d53bd5a865569390bad72f86897fc42f735d27c8
Roma3577 <mularr37@gmail.com>
2025-06-22 22:20:18 +0300

    done2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/goit-typescript-hw-02/', 
   plugins: [react()],
   build: {
     sourcemap: true,
